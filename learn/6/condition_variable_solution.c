b5613f81471b796bb4cadf23f88c55dbdadf8898
Manish <manishsurolia@gmail.com>
2021-09-14 17:58:17 +0530

    condition variable example

--- learn/6/condition_variable_solution.c
+++ learn/6/condition_variable_solution.c
@@ -5,12 +5,14 @@
 int flag = 0;
 
 pthread_mutex_t lock;
+pthread_cond_t cond;
 
 void *func1(void *p)
 {
     pthread_mutex_lock(&lock);
     while (!flag) {
         printf("T1 : Waiting for flag to become TRUE.\n");
+        pthread_cond_wait(&cond, &lock); // This will put thread to sleep, and release the lock.
         sleep(1);
     }
     printf("T1 : Processing done.\n");
@@ -25,12 +27,14 @@ void *func2(void *p)
     printf("T2 : Got the lock. Making flag as TRUE.\n");
     flag = 1;
     pthread_mutex_unlock(&lock);
+    pthread_cond_signal(&cond); // I am done with the changes, send signal.
 }
 
 int main(void)
 {
     pthread_t thread1, thread2;
     pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&cond, NULL);
 
     pthread_create(&thread1, NULL, func1, NULL);
     sleep(1); // Creating more chances of getting T1 into execution first, to showcase the issue of condition variables.
@@ -40,6 +44,7 @@ int main(void)
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
     pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&cond);
 
     return 0;
 }
