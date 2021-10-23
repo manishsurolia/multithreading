355f6c6a542740f4270ad1d78548f339e351d351
manish <manishsurolia@gmail.com>
2021-10-23 08:21:02 +0530

    Makeing barrier as a function and some README update

--- learn/puzzles/barrier/barrier.c
+++ learn/puzzles/barrier/barrier.c
@@ -3,11 +3,32 @@
 #include <semaphore.h>
 #include <stdlib.h>
 
-sem_t barrier;
-unsigned int n;                 // Total number of threads
-
-unsigned int count;
+/*
+ * Barrier related data.
+ */
+sem_t trunstile;
 sem_t mutex;                    // To protect writes to 'count'.
+unsigned int n;                 // Total number of threads
+unsigned int count;
+
+/*
+ * Barrier function.
+ */
+void barrier(void)
+{
+    sem_wait(&mutex);
+    count++;                    // Critical Section.
+    sem_post(&mutex);
+
+    if (count == n) {
+        sem_post(&trunstile);
+    }
+
+    sem_wait(&trunstile);
+    sem_post(&trunstile);
+
+    return;
+}
 
 /*
  * Symmetric code for all the threads.
@@ -15,18 +36,7 @@ sem_t mutex;                    // To protect writes to 'count'.
 void * thread_func(void *args)
 {
     printf("Executed rendezvous code.\n");
-
-    sem_wait(&mutex);
-    count++;                    // Critical Section.
-    sem_post(&mutex);
-
-    if (count == n) {
-        sem_post(&barrier);
-    }
-
-    sem_wait(&barrier);
-    sem_post(&barrier);
-
+    barrier();
     printf("Executed Critical point.\n");
     return 0;
 }
@@ -36,7 +46,7 @@ int main(void)
     pthread_t *threads;
     unsigned int temp;
 
-    sem_init(&barrier, 0, 0);   // Barrier.
+    sem_init(&trunstile, 0, 0); // Barrier.
     sem_init(&mutex, 0 , 1);    // Lock, Binary semaphore.
 
     printf("How many thread, you want to create? ");
@@ -54,7 +64,7 @@ int main(void)
         pthread_join(threads[i] , NULL);
     }
 
-    sem_destroy(&barrier);
+    sem_destroy(&trunstile);
     sem_destroy(&mutex);
 
     return 0;
