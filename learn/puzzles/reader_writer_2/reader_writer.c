262b73c63016d21ff59171cf5b763ef720678707
manish <manishsurolia@gmail.com>
2021-10-23 16:48:20 +0530

    Fix writer starvation problem with a trunstile

--- learn/puzzles/reader_writer_2/reader_writer.c
+++ learn/puzzles/reader_writer_2/reader_writer.c
@@ -1,24 +1,19 @@
 /*
- * This Program is to show how to synchronize the reading and writing by
- * multiple threads. Some threads, write the buffer, some read the buffer.
- * Readers never change any value of buffer. Hence more than one reader is
- * allowed to read the buffer.
+ * This Program is an extension to the last reader-writer problem. Please check
+ * the last reader-writer first before proceeding further.
  *
- * Writers write to the buffer, Hence writer need to take lock. Also the first
- * reader also need to take writer lock, so that writer can not take it and
- * readers leave it when all the readers are done.
+ * If a writer arrives while there are readers in the critical section, it might
+ * wait in queue forever while readers come and go. As long as a new reader
+ * arrives before the last of the current readers departs, there will always be
+ * at least one reader in the room.
  *
- * With this program we are able to synchronize the writers and readers, but
- * now we face the issue of starvation for writer.
- * Suppose there is only one writer(writing infinitely) and two
- * readers(reading infinitely).
- *
- * Once readers start reading, they don't allow the writer thread to write
- * because there is no read lock, so readers simply come and read. Only a global
- * lock is used, so there is very less contention.
- *
- * check the code below understand the logic.
+ * This situation is not a deadlock, because some threads are making progress,
+ * but it is not exactly desirable. A program like this might work as long as
+ * the load on the system is low, because then there are plenty of opportunities
+ * for the writers. But as the load increases the behavior of the system would
+ * deteriorate quickly (at least from the point of view of writers).
  */
+
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
@@ -27,24 +22,60 @@
 
 unsigned int buffer;
 
-sem_t writer_lock;
+sem_t wr_mutex;
 sem_t mutex;
 int readers;
+sem_t turnstile;
 
-sem_t barrier;
+void writer_lock(void)
+{
+    sem_wait(&turnstile);
+    sem_wait(&wr_mutex);
+    return;
+}
+
+void writer_unlock(void)
+{
+    sem_post(&wr_mutex);
+    sem_post(&turnstile);
+    return;
+}
+
+void reader_lock(void)
+{
+    sem_wait(&turnstile);
+    sem_wait(&mutex);
+    if (!readers) {              // First reader takes the write lock.
+        sem_wait(&wr_mutex);
+    }
+    readers++;
+    sem_post(&mutex);
+    sem_post(&turnstile);
+    return;
+}
+
+void reader_unlock(void)
+{
+    sem_wait(&mutex);
+    readers--;
+    if (!readers) {
+        sem_post(&wr_mutex);
+    }
+    sem_post(&mutex);
+    return;
+}
 
 /*
  * No more than one reader is allowed at a time.
  */
 void *writer_func(void *args)
 {
-    while(1) {
-        sem_wait(&writer_lock);
+    for (int i = 0; i < 2; i++) {
+        writer_lock();
         buffer = rand() % 100;
-        printf("Writer : Written value to buffer : %d\n", buffer);
-        sleep(1);
-        sem_post(&writer_lock);
-        sem_post(&barrier);
+        printf("Writer%s : Written value to buffer : %d\n", (char *)args,
+                buffer);
+        writer_unlock();
     }
     return NULL;
 }
@@ -56,59 +87,50 @@ void *writer_func(void *args)
  */
 void *reader_func(void *args)
 {
-    while (1) {
-        sem_wait(&barrier); // Barrier.
-
-        sem_wait(&mutex);
-        if (!readers) {              // First reader takes the write lock.
-            sem_wait(&writer_lock);
-        }
-        readers++;
-        sem_post(&mutex);
-
+    for (int i = 0; i < 2; i++) {
+        reader_lock();
         /*
          * Here, its just a small buffer reading for this example.
-         * But it can be anything like reading a link list, or buffer, or anything.
+         * But it can be anything like reading a link list, or buffer, or
+         * anything.
          * Readers just read the value, They don't write.
          */
         printf("Reader%s : Read value from buffer : %d\n", (char *) args,
                 buffer);
-        sleep(1);
-
-        sem_wait(&mutex);
-        readers--;
-        if (!readers) {
-            sem_post(&writer_lock);
-        }
-        sem_post(&mutex);
+        reader_unlock();
     }
     return NULL;
 }
 
 int main(void)
 {
-    pthread_t writer;
-    pthread_t reader1, reader2;
+    pthread_t writer1, writer2;
+    pthread_t reader1, reader2, reader3, reader4, reader5, reader6, reader7;
 
     buffer = 0;     // Initialize the buffer value.
     readers = 0;    // Initially, there wont be any reader.
 
-    sem_init(&writer_lock, 0, 1);
+    sem_init(&wr_mutex, 0, 1);
     sem_init(&mutex, 0, 1);
+    sem_init(&turnstile, 0, 1);
 
-    sem_init(&barrier, 0, 0);
+    pthread_create(&writer1, NULL, writer_func, "1");
+    pthread_create(&writer2, NULL, writer_func, "2");
 
-    pthread_create(&writer, NULL, writer_func, NULL);
     pthread_create(&reader1, NULL, reader_func, "1");
     pthread_create(&reader2, NULL, reader_func, "2");
+    pthread_create(&reader3, NULL, reader_func, "3");
+    pthread_create(&reader4, NULL, reader_func, "4");
+    pthread_create(&reader5, NULL, reader_func, "5");
 
-    pthread_join(writer, NULL);
+    pthread_join(writer1, NULL);
+    pthread_join(writer2, NULL);
+
     pthread_join(reader1, NULL);
     pthread_join(reader2, NULL);
-
-    sem_destroy(&writer_lock);
-    sem_destroy(&mutex);
-    sem_destroy(&barrier);
+    pthread_join(reader3, NULL);
+    pthread_join(reader4, NULL);
+    pthread_join(reader5, NULL);
 
     return 0;
 }
