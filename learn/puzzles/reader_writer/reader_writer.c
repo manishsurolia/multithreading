9c66f541787705810e20dae6ca7512c0d244c380
manish <manishsurolia@gmail.com>
2021-10-23 16:14:26 +0530

    reader-writer concurrency

--- learn/puzzles/reader_writer/reader_writer.c
+++ learn/puzzles/reader_writer/reader_writer.c
@@ -4,20 +4,12 @@
  * Readers never change any value of buffer. Hence more than one reader is
  * allowed to read the buffer.
  *
- * Writers write to the buffer, Hence writer need to take lock. Also the first
- * reader also need to take writer lock, so that writer can not take it and
- * readers leave it when all the readers are done.
+ * Writers write to the buffer, Hence writer need to take a lock, called
+ * 'writer's lock'. Also the first reader also need to take 'writer's lock', so
+ * that writer can not take it while any reader is reading the data and readers
+ * release this 'writer lock', when all the readers are done with reading.
  *
- * With this program we are able to synchronize the writers and readers, but
- * now we face the issue of starvation for writer.
- * Suppose there is only one writer(writing infinitely) and two
- * readers(reading infinitely).
- *
- * Once readers start reading, they don't allow the writer thread to write
- * because there is no read lock, so readers simply come and read. Only a global
- * lock is used, so there is very less contention.
- *
- * check the code below understand the logic.
+ * check below code to understand the logic.
  */
 #include <stdio.h>
 #include <pthread.h>
@@ -27,21 +19,55 @@
 
 unsigned int buffer;
 
-sem_t writer_lock;
+sem_t wr_mutex;
 sem_t mutex;
 int readers;
+
+void writer_lock(void)
+{
+    sem_wait(&wr_mutex);
+    return;
+}
+
+void writer_unlock(void)
+{
+    sem_post(&wr_mutex);
+    return;
+}
+
+void reader_lock(void)
+{
+    sem_wait(&mutex);
+    if (!readers) {              // First reader takes the write lock.
+        sem_wait(&wr_mutex);
+    }
+    readers++;
+    sem_post(&mutex);
+    return;
+}
+
+void reader_unlock(void)
+{
+    sem_wait(&mutex);
+    readers--;
+    if (!readers) {
+        sem_post(&wr_mutex);
+    }
+    sem_post(&mutex);
+    return;
+}
 
 /*
  * No more than one reader is allowed at a time.
  */
 void *writer_func(void *args)
 {
-    while(1) {
-        sem_wait(&writer_lock);
+    for (int i = 0; i < 2; i++) {
+        writer_lock();
         buffer = rand() % 100;
-        printf("Writer : Written value to buffer : %d\n", buffer);
-        sleep(1);
-        sem_post(&writer_lock);
+        printf("Writer%s : Written value to buffer : %d\n", (char *)args,
+                buffer);
+        writer_unlock();
     }
     return NULL;
 }
@@ -53,51 +79,49 @@ void *writer_func(void *args)
  */
 void *reader_func(void *args)
 {
-    while (1) {
-        sem_wait(&mutex);
-        if (!readers) {              // First reader takes the write lock.
-            sem_wait(&writer_lock);
-        }
-        readers++;
-        sem_post(&mutex);
-
+    for (int i = 0; i < 2; i++) {
+        reader_lock();
         /*
          * Here, its just a small buffer reading for this example.
-         * But it can be anything like reading a link list, or buffer, or anything.
+         * But it can be anything like reading a link list, or buffer, or
+         * anything.
          * Readers just read the value, They don't write.
          */
         printf("Reader%s : Read value from buffer : %d\n", (char *) args,
                 buffer);
-        sleep(1);
-
-        sem_wait(&mutex);
-        readers--;
-        if (!readers) {
-            sem_post(&writer_lock);
-        }
-        sem_post(&mutex);
+        reader_unlock();
     }
     return NULL;
 }
 
 int main(void)
 {
-    pthread_t writer;
-    pthread_t reader1, reader2;
+    pthread_t writer1, writer2;
+    pthread_t reader1, reader2, reader3, reader4, reader5, reader6, reader7;
 
     buffer = 0;     // Initialize the buffer value.
     readers = 0;    // Initially, there wont be any reader.
 
-    sem_init(&writer_lock, 0, 1);
+    sem_init(&wr_mutex, 0, 1);
     sem_init(&mutex, 0, 1);
 
-    pthread_create(&writer, NULL, writer_func, NULL);
+    pthread_create(&writer1, NULL, writer_func, "1");
+    pthread_create(&writer2, NULL, writer_func, "2");
+
     pthread_create(&reader1, NULL, reader_func, "1");
     pthread_create(&reader2, NULL, reader_func, "2");
+    pthread_create(&reader3, NULL, reader_func, "3");
+    pthread_create(&reader4, NULL, reader_func, "4");
+    pthread_create(&reader5, NULL, reader_func, "5");
 
-    pthread_join(writer, NULL);
+    pthread_join(writer1, NULL);
+    pthread_join(writer2, NULL);
+
     pthread_join(reader1, NULL);
     pthread_join(reader2, NULL);
+    pthread_join(reader3, NULL);
+    pthread_join(reader4, NULL);
+    pthread_join(reader5, NULL);
 
     return 0;
 }
